1b0061554d4363a0297e913f13a7b58ed8023933
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-11 16:39:23 +0200

    game.js

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
+import Box from "../Box";
 
 function Game() {
+  const [turn, setTurn] = useState("X");
+
+  function changeTurn() {
+    setTurn((turn) => (turn === "X" ? "O" : "X"));
+  }
   return (
-   <div id="game">
-    <div className='row'>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
+    <div id="game">
+      <div className="row">
+        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box currentState={turn} changeTurn={changeTurn} />
+      </div>
+      <div className="row">
+        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box currentState={turn} changeTurn={changeTurn} />
+      </div>
+      <div className="row">
+        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box currentState={turn} changeTurn={changeTurn} />
+      </div>
     </div>
-    <div className='row'>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
-    </div>
-    <div className='row'>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
-    </div>
-   </div>
-  )
+  );
 }
 
-export default Game
+export default Game;
