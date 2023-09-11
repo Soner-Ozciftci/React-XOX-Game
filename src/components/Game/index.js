204dd25f30b436182ee38022d560a6e3e2f4ce3b
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-11 16:56:49 +0200

    row,col

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,28 +2,31 @@ import React, { useState } from "react";
 import "./style.css";
 import Box from "../Box";
 
+const board = [[],[],[]]
+
 function Game() {
   const [turn, setTurn] = useState("X");
 
-  function changeTurn() {
+  function changeTurn(row,col) {
+    board[row][col]=turn;
     setTurn((turn) => (turn === "X" ? "O" : "X"));
   }
   return (
     <div id="game">
       <div className="row">
-        <Box currentState={turn} changeTurn={changeTurn} />
-        <Box currentState={turn} changeTurn={changeTurn} />
-        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box row={0} col={0} currentState={turn} changeTurn={changeTurn} />
+        <Box row={0} col={1} currentState={turn} changeTurn={changeTurn} />
+        <Box row={0} col={2} currentState={turn} changeTurn={changeTurn} />
       </div>
       <div className="row">
-        <Box currentState={turn} changeTurn={changeTurn} />
-        <Box currentState={turn} changeTurn={changeTurn} />
-        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box row={1} col={0} currentState={turn} changeTurn={changeTurn} />
+        <Box row={1} col={1} currentState={turn} changeTurn={changeTurn} />
+        <Box row={1} col={2} currentState={turn} changeTurn={changeTurn} />
       </div>
       <div className="row">
-        <Box currentState={turn} changeTurn={changeTurn} />
-        <Box currentState={turn} changeTurn={changeTurn} />
-        <Box currentState={turn} changeTurn={changeTurn} />
+        <Box row={2} col={0} currentState={turn} changeTurn={changeTurn} />
+        <Box row={2} col={1} currentState={turn} changeTurn={changeTurn} />
+        <Box row={2} col={2} currentState={turn} changeTurn={changeTurn} />
       </div>
     </div>
   );
