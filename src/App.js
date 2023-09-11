5c5f6c1890cd9c993ed821e9c7071787fec136d2
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-11 16:46:23 +0200

    onClick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css'
-import React from 'react';
-import Game from './components/Game'
+import "./App.css";
+import Game from "./components/Game";
 
 function App() {
-  return (
- <Game />
-  );
+  return <Game />;
 }
 
 export default App;
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,19 +1,18 @@
-import React ,{useState} from 'react'
+import React, { useState } from "react";
 
 function Box(props) {
-const [text,setText] = useState("")
-function toggleText(){
-if(text === ""){
-    setText(props.currentState)
-    props.changeTurn()
+  const [text, setText] = useState("");
+  function toggleText() {
+    if (text === "") {
+      setText(props.currentState);
+      props.changeTurn(props.row, props.col);
+    }
+  }
+  return (
+    <div className="box" onClick={toggleText}>
+      {text}
+    </div>
+  );
 }
 
-return(
-    <div className='box' onClick={toggleText}>{text}</div>
-)
-
-
-}
-}
-
-export default Box
+export default Box;
